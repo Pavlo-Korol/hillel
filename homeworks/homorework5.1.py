817de01fdfae0cc401be19ea29c6a0f57f69c228
pavlo korol mac <korol.work01@gmail.com>
2024-05-03 16:06:36 +0200

    .

--- homeworks/homorework5.1.py
+++ homeworks/homorework5.1.py
@@ -3,9 +3,15 @@ import keyword
 
 var_name = input('Write your variable name: ')
 if  not var_name.islower():
-    print('error')
+    if '_' in var_name:
+        print('alright')
+    else:
+        print('error')
 elif var_name[0].isdigit():
-    print('error')
+    if '_' in var_name:
+        print('alright')
+    else:
+        print('error')
 elif any(char in string.punctuation for char in var_name):
     if '_' in var_name:
         print('alright')
