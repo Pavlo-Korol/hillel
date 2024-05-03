cd76d94070bd4a8a00d1533fadfcdd42b43a5c7e
pavlo korol mac <korol.work01@gmail.com>
2024-05-03 16:07:39 +0200

    .

--- homeworks/homework5.3.py
+++ homeworks/homework5.3.py
@@ -1,12 +1,12 @@
 import string
 
-user_str = '!h^e@l;:l,o i a!m b,o%b'
+user_str = input('Write your string: ')
 
 def do_hashtag(user_str):
     for char in string.punctuation:
         user_str = user_str.replace(char,'') 
     cleaned_str = user_str.title().replace(' ','')
-    return '#' + cleaned_str[:140]  
+    return '#' + cleaned_str[:139]  
 
 
 print(do_hashtag(user_str))
