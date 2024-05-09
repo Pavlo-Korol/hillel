2a0332194086ad138aecb1428f91313a0fdddb31
pavlo korol mac <korol.work01@gmail.com>
2024-05-09 14:31:00 +0200

    .

--- homeworks/homorework6.2.py
+++ homeworks/homorework6.2.py
@@ -3,7 +3,12 @@ def format_time(seconds):
     hours, seconds = divmod(seconds, 60 * 60)
     minutes, seconds = divmod(seconds, 60)
     
-    day_str = 'день' if days == 1 else 'днів'
+    if days == 1:
+        day_str = 'день'
+    elif 1 < days <= 4:
+        day_str = 'дні'
+    else:
+        day_str = 'днів'
     
     formatted_time = f"{days} {day_str}, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
     return formatted_time
@@ -11,15 +16,8 @@ def format_time(seconds):
 
 seconds_input = int(input("Введіть кількість секунд: "))
 
-
 if seconds_input < 0 or seconds_input > 8640000:
     print("Неправильне значення. Введіть число більше або дорівнює 0 і менше ніж 8640000.")
 else:
-    
     formatted_result = format_time(seconds_input)
     print("Час у читабельному форматі:", formatted_result)
-
-
-
-
-
